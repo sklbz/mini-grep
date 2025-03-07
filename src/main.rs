fc66e0045f08b13775ebcb9fbfa5357ef214a22d
sklbz <sokol.bozanic@tumo.world>
2025-03-07 21:34:13 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
 mod config;
 
 use config::Config;
-use std::{env, fs, process};
+use std::{env, error::Error, fs, process::exit};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     // let Config { query, file_path } = parse_config(&args);
-    let Config { query, file_path } = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
-        process::exit(1);
+        exit(1);
     });
 
-    println!("Searching for {query} in {file_path}");
+    run(&config).unwrap_or_else(|err| {
+        println!("Application error: {err}");
+        exit(1);
+    });
+}
+
+fn run(config: &Config) -> Result<(), Box<dyn Error>> {
+    let Config { query, file_path } = &config;
+
+    println!("Searching for {query}");
+    println!("In file {file_path}");
 
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-    println!("With text:\n{contents}");
+
+    println!("With text: {contents}");
+
+    Ok(())
 }
