392f2e3d073f0f260f58354d4cafffa93138db6a
sklbz <sokol.bozanic@tumo.world>
2025-03-07 21:25:49 +0100

    update

---
+++ src/config.rs
@@ -0,0 +1,27 @@
+#[allow(dead_code)]
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+#[allow(dead_code)]
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+
+#[allow(dead_code)]
+pub fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-use std::{env, fs};
+mod config;
+
+use config::Config;
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, file_path) = parse_config(&args);
+    // let Config { query, file_path } = parse_config(&args);
+    let Config { query, file_path } = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {query} in {file_path}");
 
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     println!("With text:\n{contents}");
-}
-
-fn parse_config(args: &[String]) -> (&str, &str) {
-    return (&args[1], &args[2]);
 }
